5dacf63022ebe34f1b0ddc36697729a58bf40386
leos <leonardo.siri.email@gmail.com>
2019-10-23 14:54:43 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/* Esercizio: lista di cognomi
+
+DESCRIZIONE:
+- Chiedi all’utente il cognome
+- inseriscilo in un array con altri cognomi
+- stampa la lista ordinata alfabeticamente
+- scrivi anche la posizione della lista in cui il nuovo utente si trova
+
+QUINDI OUTPUT sarà:
+- array disordinato di partenza
+- array ordinato;
+-  posizione in cui si trova il cognome dell’utente nell’array ordinato. */
+
